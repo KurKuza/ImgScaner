a5fe52e02cb64e5a6e60d791132f378f94f363da
Kurkuza <danilsherrbakov1993@gmail.com>
2022-07-14 17:38:56 +0300

    add russian

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from PIL import Image
 img = Image.open('./img/photo-timetable.png')
 
 # For Windows
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 # ----
-tessdata_dir_config = r'--tessdata-dir "C:\Program Files (x86)\Tesseract-OCR\tessdata"'
+custom_config = r'--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'
+# custom_config = r'--oem 3 --psm 6'
 
-text = pytesseract.image_to_string(img, lang= 'rus', config=tessdata_dir_config)
+text = pytesseract.image_to_string(img, lang= 'rus', config=custom_config)
 print(text)
